f43df2a08714508cf129d5021d704964984352e2
VikasKR123 <vikas.kr@sigmoidanalytics.com>
2024-10-21 19:14:56 +0530

    github token added and automate task with github

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,4 @@ RUN pip install -r requirements.txt
 # Expose the port the Flask app will run on
 EXPOSE 5000
 
-# Command to run the application
 CMD ["python3", "app.py"]
--- monitor.py
+++ monitor.py
@@ -5,8 +5,33 @@ from data import generate_data
 from model_training import train_and_log_model
 import numpy as np
 
+import requests
+import json
+
+GITHUB_REPO = "VikasKR123/automation_mlops_1"
+GITHUB_TOKEN = {{ secrets.TOKEN }}  
+WORKFLOW_FILE_NAME = "ci.yml"
+
 # Reference data (initial state, without drift)
 reference_data = generate_data(num_samples=100, drift=False)
+
+
+def trigger_github_action():
+    url = f"https://api.github.com/repos/{GITHUB_REPO}/actions/workflows/{WORKFLOW_FILE_NAME}/dispatches"
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json",
+    }
+    data = {
+        "ref": "main",  # Branch name where the workflow resides
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 204:
+        print("CI/CD triggered successfully.")
+    else:
+        print(f"Failed to trigger CI/CD: {response.content}")
+
 
 try:
     reference_model_uri = mlflow.register_model("models:/Best_Model/Production")
@@ -26,6 +51,8 @@ while True:
         print("Drift detected! Rolling back to the reference model...")
         # Rollback logic (re-register the reference model)
         mlflow.register_model(reference_model_uri, "Best_Model")
+        print("Drift detected! Triggering CI/CD...")
+        trigger_github_action()
     
 
     time.sleep(30)  
